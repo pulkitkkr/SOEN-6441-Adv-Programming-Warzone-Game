9faf795155f819339257d1ff2a7a7bc3254581d4
zalakpatel315 <zalakpatel315@gmail.com>
2021-02-10 23:45:07 +0530

    Initial logic for accepting commands (#12)
    * Initial logic for accepting commands
    
    * Added required variables for models
    
    * changed class variables of model classes for further implementation
    
    * Method Parameter Convention
    
    Co-authored-by: jhanvi0905 <arorajhanvi38@gmail.com>

--- src/main/java/Models/Country.java
+++ src/main/java/Models/Country.java
@@ -1,4 +1,35 @@
 package Models;
 
+import java.util.List;
+
 public class Country {
+	Integer d_armies;
+	Integer d_countryId;
+	String d_continentId;
+	List<Integer> d_adjacentCountryIds;
+	
+	public Integer getD_armies() {
+		return d_armies;
+	}
+	public void setD_armies(Integer p_armies) {
+		this.d_armies = p_armies;
+	}
+	public Integer getD_countryId() {
+		return d_countryId;
+	}
+	public void setD_countryId(Integer p_countryId) {
+		this.d_countryId = p_countryId;
+	}
+	public String getD_continentId() {
+		return d_continentId;
+	}
+	public void setD_continentId(String p_continentId) {
+		this.d_continentId = p_continentId;
+	}
+	public List<Integer> getD_adjacentCountryIds() {
+		return d_adjacentCountryIds;
+	}
+	public void setD_adjacentCountryIds(List<Integer> p_adjacentCountryIds) {
+		this.d_adjacentCountryIds = p_adjacentCountryIds;
+	}
 }
--- src/main/java/Models/Order.java
+++ src/main/java/Models/Order.java
@@ -1,4 +1,33 @@
 package Models;
 
 public class Order {
+	String d_orderAction;
+	Integer d_targetCountryId;
+	Integer d_sourceCountryId;
+	Integer d_numberOfArmiesToMove;
+	
+	public String getD_orderAction() {
+		return d_orderAction;
+	}
+	public void setD_orderAction(String p_orderAction) {
+		this.d_orderAction = p_orderAction;
+	}
+	public Integer getD_targetCountryId() {
+		return d_targetCountryId;
+	}
+	public void setD_targetCountryId(Integer p_targetCountryId) {
+		this.d_targetCountryId = p_targetCountryId;
+	}
+	public Integer getD_sourceCountryId() {
+		return d_sourceCountryId;
+	}
+	public void setD_sourceCountryId(Integer p_sourceCountryId) {
+		this.d_sourceCountryId = p_sourceCountryId;
+	}
+	public Integer getD_numberOfArmiesToMove() {
+		return d_numberOfArmiesToMove;
+	}
+	public void setD_numberOfArmiesToMove(Integer p_numberOfArmiesToMove) {
+		this.d_numberOfArmiesToMove = p_numberOfArmiesToMove;
+	}
 }
