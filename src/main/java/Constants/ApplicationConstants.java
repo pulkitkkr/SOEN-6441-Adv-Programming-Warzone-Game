bcb30c3a371b9e5574836f2f0fe0299588220066
Avneetpannu <Avneetpannu@localhost>
2021-02-17 20:58:56 +0530

    Adding description of constant class

--- src/main/java/Constants/ApplicationConstants.java
+++ src/main/java/Constants/ApplicationConstants.java
@@ -1,14 +1,17 @@
 package Constants;
 
+/**
+ * ApplicationConstants class to wrap constants for general utility.
+ */
 public final class ApplicationConstants {
 
 	public static final String INVALID_COMMAND_ERROR_EDITMAP = "Invalid command. Kindly provide command in Format of : editmap filename";
 	public static final String INVALID_COMMAND_ERROR_EDITCONTINENT = "Invalid command. Kindly provide command in Format of : editcontinent -add continentID continentvalue -remove continentID";
 	public static final String INVALID_COMMAND_ERROR_SAVEMAP = "Invalid command. Kindly provide command in Format of : savemap filename";
-	public static final String INVALID_MAP_ERROR_EMPTY= "No Map found! Please load a valid map to check!";
+	public static final String INVALID_MAP_ERROR_EMPTY = "No Map found! Please load a valid map to check!";
 	public static final String INVALID_COMMAND_ERROR_LOADMAP = "Invalid command. Kindly provide command in Format of : loadmap filename";
-	public static final String INVALID_COMMAND_ERROR_VALIDATEMAP= "Invalid command! validatemap is not supposed to have any arguments";
-	public static final String VALID_MAP= "The loaded map is valid!";
+	public static final String INVALID_COMMAND_ERROR_VALIDATEMAP = "Invalid command! validatemap is not supposed to have any arguments";
+	public static final String VALID_MAP = "The loaded map is valid!";
 
 	public static final String ARGUMENTS = "arguments";
 	public static final String OPERATION = "operation";
@@ -19,7 +22,9 @@ public final class ApplicationConstants {
 	public static final String COUNTRIES = "[countries]";
 	public static final String BORDERS = "[borders]";
 
-	// private constructor so it can't be instantiated.
+	/**
+	 * private constructor to prevent caller from constructing objects of this class
+	 */
 	private ApplicationConstants() {
 	}
 }
