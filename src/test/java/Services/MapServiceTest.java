1391f5ab440f32ccc64733d8288b1ce1b301ff6d
Avneetpannu <Avneetpannu@localhost>
2021-02-18 01:37:31 +0530

    Adding Unit Test cases for load Map.

--- src/test/java/Services/MapServiceTest.java
+++ src/test/java/Services/MapServiceTest.java
@@ -1,4 +1,79 @@
 package Services;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Models.Continent;
+import Models.Country;
+import Models.GameState;
+import Models.Map;
+
 public class MapServiceTest {
+	MapService d_mapservice;
+	Map d_map;
+	GameState d_state;
+
+	@Before
+	public void setup() {
+		d_mapservice = new MapService();
+		d_map = new Map();
+		d_state = new GameState();
+		d_map = d_mapservice.loadMap(d_state, "europe.map");
+	}
+
+	@Test
+	public void testContinentIdAndValues() {
+		List<Integer> l_actualContinentIdList = new ArrayList<Integer>();
+		List<Integer> l_actualContinentValueList = new ArrayList<Integer>();
+
+		List<Integer> l_expectedContinentIdList = new ArrayList<Integer>();
+		l_expectedContinentIdList.addAll(Arrays.asList(1, 2, 3, 4));
+
+		List<Integer> l_expectedContinentValueList = new ArrayList<Integer>();
+		l_expectedContinentValueList.addAll(Arrays.asList(5, 4, 5, 3));
+
+		for (Continent l_continent : d_map.getD_continents()) {
+			l_actualContinentIdList.add(l_continent.getD_continentID());
+			l_actualContinentValueList.add(l_continent.getD_continentValue());
+		}
+
+		assertEquals(l_expectedContinentIdList, l_actualContinentIdList);
+		assertEquals(l_expectedContinentValueList, l_actualContinentValueList);
+	}
+
+	@Test
+	public void testCountryIdAndNeighbors() {
+		List<Integer> l_actualCountryIdList = new ArrayList<Integer>();
+		LinkedHashMap<Integer, List<Integer>> l_actualCountryNeighbors = new LinkedHashMap<Integer, List<Integer>>();
+
+		List<Integer> l_expectedCountryIdList = new ArrayList<Integer>();
+		l_expectedCountryIdList.addAll(Arrays.asList(1, 2, 3, 4, 5));
+
+		LinkedHashMap<Integer, List<Integer>> l_expectedCountryNeighbors = new LinkedHashMap<Integer, List<Integer>>() {
+			{
+				put(1, new ArrayList<Integer>(Arrays.asList(8, 21, 6, 7, 5, 2, 3, 4)));
+				put(2, new ArrayList<Integer>(Arrays.asList(8, 1, 3)));
+				put(3, new ArrayList<Integer>(Arrays.asList(1, 2)));
+				put(4, new ArrayList<Integer>(Arrays.asList(22, 1, 5)));
+				put(5, new ArrayList<Integer>(Arrays.asList(1, 4)));
+			}
+		};
+
+		for (Country l_country : d_map.getD_countries()) {
+			ArrayList<Integer> l_neighbours = new ArrayList<Integer>();
+			l_actualCountryIdList.add(l_country.getD_countryId());
+			l_neighbours.addAll(l_country.getD_adjacentCountryIds());
+			l_actualCountryNeighbors.put(l_country.getD_countryId(), l_neighbours);
+		}
+		assertEquals(l_expectedCountryIdList, l_actualCountryIdList);
+		assertEquals(l_expectedCountryNeighbors, l_actualCountryNeighbors);
+	}
+
 }
