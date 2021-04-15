151e0ceaf002b8a842434e6c76623a805381cb71
pulkitkkr <pulkitkkr@gmail.com>
2021-04-15 16:17:29 +0530

    Handle Unallocated Armies

--- src/main/java/Models/CheaterPlayer.java
+++ src/main/java/Models/CheaterPlayer.java
@@ -5,6 +5,7 @@ package Models;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import Services.PlayerService;
 
@@ -24,10 +25,40 @@ public class CheaterPlayer extends PlayerBehaviorStrategy {
 	@Override
 	public String createOrder(Player p_player, GameState p_gameState) throws IOException {
 
+		if(p_player.getD_noOfUnallocatedArmies() != 0) {
+			while(p_player.getD_noOfUnallocatedArmies() > 0) {
+				Random l_random = new Random();
+				Country l_randomCountry = getRandomCountry(p_player.getD_coutriesOwned());
+				int l_armiesToDeploy = l_random.nextInt(p_player.getD_noOfUnallocatedArmies()) + 1;
+
+				l_randomCountry.setD_armies(l_armiesToDeploy);
+				p_player.setD_noOfUnallocatedArmies(p_player.getD_noOfUnallocatedArmies() - l_armiesToDeploy);
+
+				String l_logMessage = "Cheater Player: " + p_player.getPlayerName() +
+						" assigned " + l_armiesToDeploy +
+						" armies to  " + l_randomCountry.getD_countryName();
+
+				p_gameState.updateLog(l_logMessage, "effect");
+			}
+		}
+
 		conquerNeighboringEnemies(p_player, p_gameState);
 		doubleArmyOnEnemyNeighboredCounties(p_player, p_gameState);
 
+		p_player.checkForMoreOrders(true);
 		return null;
+	}
+
+	/**
+	 *
+	 * returns a random country owned by player
+	 *
+	 * @param p_listOfCountries list of countries owned by player
+	 * @return a random country from list
+	 */
+	private Country getRandomCountry(List<Country> p_listOfCountries){
+		Random l_random = new Random();
+		return p_listOfCountries.get(l_random.nextInt(p_listOfCountries.size()));
 	}
 
 	/**
@@ -45,6 +76,8 @@ public class CheaterPlayer extends PlayerBehaviorStrategy {
 			if(l_countryEnemies.size() == 0) continue;
 
 			Integer l_arimiesInTerritory = l_ownedCountry.getD_armies();
+
+			if(l_arimiesInTerritory == 0) continue;
 
 			l_ownedCountry.setD_armies(l_arimiesInTerritory*2);
 
