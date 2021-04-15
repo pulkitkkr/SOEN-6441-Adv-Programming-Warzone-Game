b87f6b171f30e357a61656df865a3681363fdebc
avneetpannu <Avneetpannu@localhost>
2021-04-15 14:22:29 +0530

    adding junit test cases of aggressive player.

--- src/test/java/Models/AggressivePlayerTest.java
+++ src/test/java/Models/AggressivePlayerTest.java
@@ -16,95 +16,89 @@ import org.junit.Test;
  * This class tests the working of Aggressive Player Behavior.
  */
 public class AggressivePlayerTest {
-	
+
 	/**
-	 * Object of Aggressive Player.
+	 * aggressive Player to test.
 	 */
 	Player d_player;
-	
+
 	/**
-	 * Country class object;
-	 */
-	Country d_country;
-	
-	/**
-	 * Object of GameState Class.
-	 */
-	GameState d_gameState;
-	
-	/**
-	 * Object of PlayerBehaviorStrategy Class.
+	 * Strategy of Player.
 	 */
 	PlayerBehaviorStrategy d_playerBehaviorStrategy;
-	
+
 	/**
-	 * Aggressive Player Object.
+	 * Aggressive player strategy.
 	 */
 	AggressivePlayer d_aggressivePlayer = new AggressivePlayer();
-	
+
 	/**
-     * Setup for testing Aggressive Player Behavior Strategy.
-     */
-    @Before
-    public void setup() {
-        this.d_country = new Country(1, "Spain", 1);
-        Country l_country1 = new Country(1, "Portugal", 1);
-        Country l_country2 = new Country(1, "France", 1);
-        
-        l_country1.setD_countryId(3);
-		d_country.addNeighbour(3);
+	 * Game State.
+	 */
+	GameState d_gameState = new GameState();
 
-		l_country2.setD_countryId(2);
-		d_country.addNeighbour(2);
+	/**
+	 * Country India.
+	 */
+	Country d_country1;
 
-		this.d_country.setD_armies(10);
-		l_country1.setD_armies(3);
-		l_country2.setD_armies(2);
+	/**
+	 * Setup For testing Aggressive Behavior Strategy.
+	 */
+	@Before
+	public void setup() {
+		this.d_country1 = new Country(1, "India", 1);
+		Country l_country2 = new Country(1, "China", 1);
+		Country l_country3 = new Country(1, "Pakistan", 1);
+
+		l_country2.setD_countryId(3);
+		d_country1.addNeighbour(3);
+
+		l_country3.setD_countryId(2);
+		d_country1.addNeighbour(2);
+
+		this.d_country1.setD_armies(10);
+		l_country2.setD_armies(3);
+		l_country3.setD_armies(2);
 
 		ArrayList<Country> l_list = new ArrayList<Country>();
-		l_list.add(d_country);
-		l_list.add(l_country1);
+		l_list.add(d_country1);
 		l_list.add(l_country2);
+		l_list.add(l_country3);
 
 		d_playerBehaviorStrategy = new AggressivePlayer();
-		d_player = new Player("Bhoomi");
+		d_player = new Player("Avneet");
 		d_player.setD_coutriesOwned(l_list);
 		d_player.setStrategy(d_playerBehaviorStrategy);
-		d_player.setD_noOfUnallocatedArmies(8);
+		d_player.setD_noOfUnallocatedArmies(5);
 
 		List<Player> l_listOfPlayer = new ArrayList<Player>();
 		l_listOfPlayer.add(d_player);
 
 		Map l_map = new Map();
 		l_map.setD_countries(l_list);
+		l_map.setD_countries(l_list);
 		d_gameState.setD_map(l_map);
 		d_gameState.setD_players(l_listOfPlayer);
 
-    }
-
-    /**
-     * Tests the creation of Order and deployment of the first order in the list.
-     *
-     * @throws IOException Input/Output Exception
-     */
-    @Test
-    public void testCreateOrder() throws IOException{
-        assertEquals("deploy", d_player.getPlayerOrder(d_gameState).split(" ")[0]);
-    }
-    
-    /**
-	 * Check if aggressive player deploy armies on strongest country or not.
-	 */
-	@Test
-	public void testStrongestCountry() {
-		assertEquals("Spain", d_aggressivePlayer.getStrongestCountry(d_player,d_gameState).getD_countryName());
 	}
 
 	/**
-	 * Check if aggressive player attacks to enemy territory or not.
+	 * Checks if it creates an Order String and first order is deploy.
+	 *
+	 * @throws IOException Exception
 	 */
 	@Test
-	public void testStrongestCountryWithAdjacentCountry() {
-		assertEquals(null, d_aggressivePlayer.getStrongestCountryWithAdjacentCountry(d_player, d_gameState).getD_countryName());
+	public void testOrderCreation() throws IOException {
+		assertEquals("deploy", d_player.getPlayerOrder(d_gameState).split(" ")[0]);
 	}
+
+	/**
+	 * Check if aggressive player deploy armies on weakest country or not.
+	 */
+	@Test
+	public void testStrongestCountry() {
+		assertEquals("India", d_aggressivePlayer.getStrongestCountry(d_player, d_gameState).getD_countryName());
+	}
+
 }
